8766eda42c1e60e31787b2122440df09638a5c78
Saugat <saugatg189@gmail.com>
2025-01-31 09:16:08 +0545

    zod validation added

--- meteverse/apps/http/src/index.ts
+++ meteverse/apps/http/src/index.ts
@@ -1,10 +1,12 @@
 import express from "express"
 import { router } from "./routes/v1";
+import client from "@repo/db/client"
 
 const app = express();
 
 
 app.use("/api/v1",router);
+app.use(express.json())
 
 app.listen(process.env.PORT || 3000,()=>{
     console.log(`Metaverse is running over the 3000 port`);
--- meteverse/apps/http/src/routes/v1/index.ts
+++ meteverse/apps/http/src/routes/v1/index.ts
@@ -2,10 +2,19 @@ import { Router } from "express";
 import { userRouter } from "./user";
 import { spaceRouter } from "./space";
 import { adminRouter } from "./admin";
+import { SignupSchema } from "../../types";
 
 export const router = Router();
 
 router.post('/signup',(req,res)=>{
+
+    const parsedData = SignupSchema.safeParse(req.body)
+    if(!parsedData.success){
+        res.status(400).json({
+            message:"Validation Failed"
+        })
+    }
+
     res.json({
         message:"This is the signup"})
 })
